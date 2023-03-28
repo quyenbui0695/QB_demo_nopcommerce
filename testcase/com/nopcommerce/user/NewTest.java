911cba490c864e8c0f52d247b6ec6a7581ce6fac
quyenbui0695 <quyenbui0695@gmail.com>
2023-03-28 21:00:14 +0700

    Init automation framework

---
+++ testcase/com/nopcommerce/user/NewTest.java
@@ -0,0 +1,25 @@
+package com.nopcommerce.user;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class NewTest {
+ 
+  @BeforeClass
+  public void beforeClass() {
+  }
+  @Test
+  public void f() {
+  }
+  @AfterClass
+  public void afterClass() {
+  }
+
+
+
+}
